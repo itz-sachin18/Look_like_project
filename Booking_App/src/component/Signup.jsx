b83e24c2c3e9b4a6e59894691135abff30f836c0
itz-sachin18 <sachin.v2023it@sece.ac.in>
2025-10-07 12:18:56 +0530

    deploy

--- Booking_App/src/component/Signup.jsx
+++ Booking_App/src/component/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./SignUp.css";
+import "./Signup.css";
 import { useNavigate } from "react-router-dom";
 import BASE_URL from "../api";
 

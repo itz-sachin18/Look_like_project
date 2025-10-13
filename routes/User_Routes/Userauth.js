30f0852839abea6382468e4fef4a2aa139f5ab35
itz-sachin18 <sachin.v2023it@sece.ac.in>
2025-10-13 12:35:15 +0530

    deploy

--- routes/User_Routes/Userauth.js
+++ routes/User_Routes/Userauth.js
@@ -50,7 +50,15 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid phone number or password' });
     }
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.cookie('token', token, { httpOnly: true, secure: false, sameSite: 'Lax' });
+    const isProduction = process.env.NODE_ENV === "production";
+
+ res.cookie('token', token, {
+  httpOnly: true,
+  secure: isProduction, // true in production (HTTPS)
+  sameSite: isProduction ? 'None' : 'Lax',
+  maxAge: 60 * 60 * 1000, // 1 hour
+});
+
     res.status(200).json({ message: 'Login successful' ,userId: user._id,});
   } catch (err) {
     res.status(500).json({ message: 'Server error' });

98ac6c443ae13b489c532f36ae37b70d71f91b3b
itz-sachin18 <sachin.v2023it@sece.ac.in>
2025-10-13 11:32:47 +0530

    deploy

--- Booking_App/vite.config.js
+++ Booking_App/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
-     outDir: 'dist'   // <-- Add this line to output to 'build' folder
+    outDir: 'build'  // <-- Vercel will now find this folder
   }
 })

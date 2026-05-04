86eb5beb26f0bf4b1e7394dde09a016d6b0ade27
itz-sachin18 <sachin.v2023it@sece.ac.in>
2026-05-04 10:29:07 +0530

    new v1.2

--- Booking_App/vite.config.js
+++ Booking_App/vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
   build: {
-    outDir: 'build',  // <-- Add this line to output to 'build' folder
+    outDir: "build", // <-- Add this line to output to 'build' folder
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  },
+});
